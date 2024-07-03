9e791825721d6e5df29afccee92e9e68b1f6a83f
Awan Abdillah <awanabdill@gmail.com>
2024-07-03 14:57:05 +0700

    Some change

--- middlewares/auth-middleware.ts
+++ middlewares/auth-middleware.ts
@@ -5,27 +5,27 @@ import { UserData } from '../models/user';
 const jwt = require('jsonwebtoken');
 
 export function authLevelRecruiter(req: Request, res: Response, next: NextFunction) {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
 
-    if (token == null) return res.sendStatus(401);
+  if (token == null) return res.sendStatus(401);
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err: string, data: UserData) => {
-        if (err || data.role !== 'recruiter') return res.sendStatus(403);
-        req.user = { id: data.id, role: data.role };
-        next();
-    });
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err: string, data: UserData) => {
+    if (err || data.role !== 'recruiter') return res.sendStatus(403);
+    req.user = { id: data.id, role: data.role };
+    next();
+  });
 }
 
 export function authLevelApplier(req: Request, res: Response, next: NextFunction) {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
 
-    if (token == null) return res.sendStatus(401);
+  if (token == null) return res.sendStatus(401);
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err: string, data: UserData) => {
-        if (err) return res.sendStatus(403);
-        req.user = { id: data.id, role: data.role };
-        next();
-    });
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err: string, data: UserData) => {
+    if (err) return res.sendStatus(403);
+    req.user = { id: data.id, role: data.role };
+    next();
+  });
 }
